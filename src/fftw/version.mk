79f9d52b6e42a10a3547d15aec24f2c5dcd67fff
Trevor Cooper <tcooper@sdsc.edu>
2013-11-25 14:45:54 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/fftw/version.mk
+++ src/fftw/version.mk
@@ -1,3 +1,14 @@
-NAME    = fftw_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
-VERSION = 3.3.3
-RELEASE = 0
+NAME               = fftw_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
+VERSION            = 3.3.3
+RELEASE            = 0
+PKGROOT            = /opt/fftw/$(VERSION)/$(ROLLCOMPILER)/$(ROLLMPI)/$(ROLLNETWORK)
+
+SRC_SUBDIR         = fftw
+
+SOURCE_NAME        = fftw
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
