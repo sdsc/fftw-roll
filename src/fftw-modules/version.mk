87f16c4400407ad90ee86da2fcfbaa4f917c34ce
Jim Hayes <jhayes@sdsc.edu>
2014-09-26 16:16:48 -0700

    Modulefile rpm build restructuring.
    Adopt/adapt Rocks approach to copying version from corresponding s/w package.
    Move all variant definitions into version.mk so that identical modulefile
    Makefile will work across rolls.

--- src/fftw-modules/version.mk
+++ src/fftw-modules/version.mk
@@ -3,8 +3,14 @@ ifndef ROLLCOMPILER
 endif
 COMPILERNAME := $(firstword $(subst /, ,$(ROLLCOMPILER)))
 
-NAME       = fftw-modules_$(COMPILERNAME)
-VERSION    = 3.3.3
-RELEASE    = 5
+NAME        = fftw-modules_$(COMPILERNAME)
+RELEASE     = 7
+PKGROOT     = /opt/modulefiles/applications/.$(COMPILERNAME)/fftw
 
-RPM.EXTRAS = AutoReq:No
+VERSION_SRC = $(REDHAT.ROOT)/src/fftw/version.mk
+VERSION_INC = version.inc
+include $(VERSION_INC)
+
+VERSION_ADD = 2.1.5
+
+RPM.EXTRAS  = AutoReq:No
